95d2ce2cea2eea6c0eaa2d8a8e991926d7fd99cb
sripragnaneti <145009098+sripragnaneti@users.noreply.github.com>
2026-04-13 13:30:00 +0530

    Delete test/german.py

--- test/german.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-english_text = "Write a Python program that calculates the factorial of a number."
-
-german_text = "Schreibe ein Python-Programm, das die Fakultät einer Zahl berechnet."
-
-tts = gTTS(text=german_text, lang='de')
-tts.save("german_output.mp3")
